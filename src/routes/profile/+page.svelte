b3cf1c46a6e5203ef1bda0b931108ad158e492bd
me me <106704354+thisiscoding1234@users.noreply.github.com>
2023-11-24 15:56:53 +0000

    an unignoreable commit...

---
+++ src/routes/profile/+page.svelte
@@ -0,0 +1,4 @@
+<h1> User's profile</h1>
+<img src="https://api.dicebear.com/7.x/rings/svg?seed=user&radius=50&backgroundType=gradientLinear&ringColor=4db6ac,81c784,9575cd,aed581,ba68c8,e57373,f06292,ff8a65,ffb74d,ffd54f,dce775,4dd0e1,7986cb&ringFive=full,half,quarter,eighth&ringFour=half,quarter,full,eighth&ringOne=half,quarter,full,eighth&ringThree=half,quarter,full,eighth&ringTwo=half,quarter,full,eighth&backgroundColor=c0aede,b6e3f4" alt="profile picture of user: test" />
+
+<h2>status: testing. src/routes/authenticate/+page.svelte</h2>
